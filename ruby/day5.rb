6cfd78f3aa2f1478e5239d3a7c1669fe1f8bc9b6
Stefano Cobianchi <stefano@trustlayer.io>
2022-12-06 08:45:34 +0100

    Day 5 part 2 (Ruby)

--- ruby/day5.rb
+++ ruby/day5.rb
@@ -2,21 +2,18 @@ def input
   @input ||= File.read('../inputs/day5.txt')
 end
 
-def stacks
-  @stacks ||=
-    begin
-      rows = input.split("\n\n")[0].split("\n")
-      num = rows[-1].split.size
-      res = Array.new(num) { [] }
-      rows[0...-1].reverse.each do |row|
-        crates = "#{row} ".scan(/((\[(\w)\])|\s{3}) /)
-        (0...num).each do |col|
-          crate = crates[col][2]
-          res[col] << crate if crate
-        end
-      end
-      res
+def get_stacks
+  rows = input.split("\n\n")[0].split("\n")
+  num = rows[-1].split.size
+  res = Array.new(num) { [] }
+  pat = /\[(.)\]/
+  rows[0...-1].reverse.each do |row|
+    num.times do |n|
+      crate = row[(n * 4)...(n * 4) + 4]
+      res[n] << Regexp.last_match[1] if crate =~ pat
     end
+  end
+  res
 end
 
 def moves
@@ -25,6 +22,7 @@ end
 
 def part1
   pat = /move (\d+) from (\d+) to (\d+)/
+  stacks = get_stacks
   moves.each do |line|
     move = pat.match(line)
     move[1].to_i.times do
@@ -34,4 +32,18 @@ def part1
   stacks.map(&:last).join
 end
 
+def part2
+  pat = /move (\d+) from (\d+) to (\d+)/
+  stacks = get_stacks
+  moves.each do |line|
+    move = pat.match(line)
+    qty = move[1].to_i
+    src = move[2].to_i - 1
+    dst = move[3].to_i - 1
+    stacks[dst].concat(stacks[src].pop(qty))
+  end
+  stacks.map(&:last).join
+end
+
 puts part1
+puts part2
